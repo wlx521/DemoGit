eb83e13f9b7f7054ba2614a17829811f01e3a47b
wlx521 <2141423129@qq.com>
2024-09-19 17:29:31 +0800

    这是一个小测试

---
+++ .idea/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author 沉默小吴
+ * @Date 2024/9/19 17:24
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
